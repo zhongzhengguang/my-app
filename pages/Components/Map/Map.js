39233617b68b97dc538486a9087ead05d266aa78
鍾政廣 <paul930237@gmail.com>
2022-10-31 22:59:51 +0800

    map page test4

--- pages/Components/Map/Map.js
+++ pages/Components/Map/Map.js
@@ -5,20 +5,18 @@ import "leaflet/dist/leaflet.css";
 
 const Map = () => {
   return (
-    <div>
-      <div className="z-[-1] object-cover absolute top-0 left-0 w-full h-full">
-        <MapContainer
-          center={[25.058452802073912, 121.56717401111011]}
-          zoom={10}
-          scrollWheelZoom={true}
-          className="w-full h-full"
-        >
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-        </MapContainer>
-      </div>
+    <div className="z-[-1] object-cover absolute top-0 left-0 w-full h-full">
+      <MapContainer
+        center={[25.058452802073912, 121.56717401111011]}
+        zoom={10}
+        scrollWheelZoom={true}
+        className="w-full h-full"
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
     </div>
   );
 };
--- pages/map.js
+++ pages/map.js
@@ -1,9 +1,7 @@
 import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
+import Map from "./Components/Map";
 
-import dynamic from "next/dynamic";
-
-const Map = dynamic(() => import("./Components/Map/Map"), { ssr: false });
 const map = () => {
   return (
     <div>
