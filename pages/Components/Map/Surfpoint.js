cf20525a899c960a3bd73f3e4f5c2bc784f7f3bd
鍾政廣 <paul930237@gmail.com>
2022-11-01 15:07:21 +0800

    add mapbox test

--- pages/Components/Map/Surfpoint.js
+++ pages/Components/Map/Surfpoint.js
@@ -1,31 +1,37 @@
-import * as React from "react";
-import Map, { Marker } from "react-map-gl";
+import { React, useState } from "react";
+import Map, { Marker, Popup } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
+import { MapboxPopup } from "react-map-gl/dist/esm/types";
 const MAPBOX_TOKEN =
   "pk.eyJ1Ijoiemhvbmd6aGVuZ2d1YW5nIiwiYSI6ImNsOXd4d29sYjA1OXozbnBhNnI1bm9xcnkifQ.ijReFc9-sxVsHcbzagb-0w";
 
 let Surfpoints = [
   {
+    id: 1,
     name: "honeyBay",
     latitude: [24.933169593789774],
     longitude: [121.88572098227161],
   },
   {
+    id: 2,
     name: "Wushi",
     latitude: [24.87135903984007],
     longitude: [121.84231049225696],
   },
   {
+    id: 3,
     name: "Jinzun ",
     latitude: [22.956187473143952],
     longitude: [121.29513265045756],
   },
   {
+    id: 4,
     name: "Fulong ",
     latitude: [25.023013918450737],
     longitude: [121.94294856015472],
   },
   {
+    id: 5,
     name: "Jishan ",
     latitude: [25.23941638517633],
     longitude: [121.63284421111359],
@@ -33,6 +39,8 @@ let Surfpoints = [
 ];
 
 const Surfpoint = () => {
+  const [showPopup, setShowPopup] = useState();
+  console.log(showPopup.longitude);
   return (
     <Map
       initialViewState={{
@@ -44,19 +52,22 @@ const Surfpoint = () => {
       mapStyle="mapbox://styles/zhongzhengguang/cl9wy5dyz000d14qqxmm85712"
       mapboxAccessToken={MAPBOX_TOKEN}
     >
-      {Surfpoints.map((item) => (
-        <Marker
-          key={item.id}
-          latitude={item.latitude}
-          longitude={item.longitude}
-          color="red"
-        />
-      ))}
-      <Marker
-        latitude={25.058452802073912}
-        longitude={121.56717401111011}
-        color="red"
-      />
+      <div>
+        {Surfpoints.map((item) => (
+          <Marker
+            key={item.id}
+            latitude={item.latitude}
+            longitude={item.longitude}
+          >
+            <p
+              className="cursor-pointer text-2xl"
+              onClick={() => setShowPopup(item)}
+            >
+              🏄‍♀️
+            </p>
+          </Marker>
+        ))}
+      </div>
     </Map>
   );
 };
