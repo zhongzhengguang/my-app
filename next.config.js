1fd81d18e5a36f2fd1445f1c43f459f6b0c47396
鍾政廣 <paul930237@gmail.com>
2022-11-01 13:22:55 +0800

    add mapbox test

--- next.config.js
+++ next.config.js
@@ -4,14 +4,6 @@ const nextConfig = {
   swcMinify: true,
 };
 
-module.exports = {
-  reactStrictMode: true,
-  env: {
-    MAPBOX_ACCESS_TOKEN: "[MAPBOX_TOKEN]",
-  },
-};
-
-// next.config.js
 const withVideos = require("next-videos");
 
 module.exports = withVideos();
--- pages/map.js
+++ pages/map.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Map1 from "./Components/Map/Map";
 import Navbar from "./Components/Navbar/Navbar";
 
 const map = () => {
   return (
     <div>
       <Navbar />
+      <Map1 />
     </div>
   );
 };
